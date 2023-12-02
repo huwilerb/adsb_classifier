1cdeaa2bb5c19168135aae9d9e828ba21f67211e
huwilerb <huwilerb@hotmail.ch>
2023-12-02 17:27:01 +0100

    feat: function to clean the grounded data

--- src/features/features.py
+++ src/features/features.py
@@ -1,2 +1,13 @@
-class Test:
-    pass 
+import polars as pl 
+
+def clean_grounded(df: pl.LazyFrame) -> pl.LazyFrame:
+    return (
+        df
+        .filter(
+            (~(((pl.col("grounded") != pl.col("grounded")
+                .shift(-1))
+                .cast(pl.Int8)
+                .cumsum() == 0)         
+                & (pl.col("grounded").shift(-1))))
+        )
+    )
